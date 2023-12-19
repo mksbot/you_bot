13d677712f0ccab97b25f3c839273da19e61c9c0
mksbot <81396341+mksbot@users.noreply.github.com>
2023-12-18 22:00:40 -0300

    Update arquivos_texto.py

--- fun/arquivos_texto.py
+++ fun/arquivos_texto.py
@@ -3,7 +3,7 @@ def registro(registrar, nome='arq01', substituir="sim"):
         s = 'w'
     else:
         s = 'a'
-    with open(f'{nome}.txt', f'{s}', encoding="utf-8") as arquivo:
+    with open(f'{nome}.txt', f'{s}') as arquivo:
         if s == 'w':
             arquivo.write(f'{registrar}')
         else:
@@ -12,7 +12,7 @@ def registro(registrar, nome='arq01', substituir="sim"):
 
 
 def abrir_reg(nome="arq01"):
-        with open(f'{nome}.txt',encoding="utf-8") as arquivo:
+        with open(f'{nome}.txt') as arquivo:
             b = (arquivo.read())
             return b
 
